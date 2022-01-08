82058c623bd2e156d25e454a151455c42bcf9456
Egemen Can Guler <egemencanguler@gmail.com>
2022-01-08 23:31:25 +0300

    only export task folders

--- Assets/Utils/Editor/MenuItems.cs
+++ Assets/Utils/Editor/MenuItems.cs
@@ -7,12 +7,8 @@ public static class MenuItems
    [MenuItem("Dalak/ExportSolution")]
    static void ExportSolution()
    {
-      List<string> paths = new List<string>();
-      paths.AddRange(Directory.GetDirectories("Assets/Task0"));
-      paths.AddRange(Directory.GetDirectories("Assets/Task1"));
-
       string fileName = $"Assets/MySolutions.unitypackage";
-      AssetDatabase.ExportPackage(paths.ToArray(),fileName, ExportPackageOptions.Recurse);
+      AssetDatabase.ExportPackage(new []{"Assets/Task0","Assets/Task1"},fileName, ExportPackageOptions.Recurse);
       AssetDatabase.ImportAsset(fileName);
       AssetDatabase.Refresh();
    }
