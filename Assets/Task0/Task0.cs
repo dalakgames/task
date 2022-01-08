7ffffb4d16190f7f93139655e075320a1cd6657a
Egemen Can Guler <egemencanguler@gmail.com>
2022-01-08 23:13:07 +0300

    task0 more explanation

--- Assets/Task0/Task0.cs
+++ Assets/Task0/Task0.cs
@@ -5,7 +5,19 @@ public class Task0 : MonoBehaviour
 {
     /*
      ----- TASK 0 ----------
-     * sizden Assets/task0 videosunundaki kuralları izleyen bir proje hazırlamanızı istiyoruz.
+     *
+      sizden Assets/task0 videosunundaki kuralları izleyen bir proje hazırlamanızı istiyoruz.
+      
+      Fare sol tuşuna basıldığında  farenin oldugu noktada bir yuvarlak oluşur
+      
+      Yuvarlak fare ile birlikte hareket etmeli ve zamanla büyümelidir 
+      
+      Kontrol edilen yuvarlak başka bir yuvarlak ile temas ediyorsa kırmızı etmiyorsa
+      yeşil renkte olmalıdır
+      
+      Fare tuşu bırakıldığında yuvarlak başka bir yuvarlakla temas etmiyorsa sahnede kalmaya devam eder
+      ediyorsa yok olur.
+     
      * TaskUtils içerisinde bulunan input ve draw fonksyonlarını kullanabilirsiniz.
      * Çözümünüzüm tamamını bu scriptin içerisine yazmalisiniz.
      * Vector2,Vector3,Mathf,System.Collections dışında olabildiğince az disaridan kod kullanmaya calisin
